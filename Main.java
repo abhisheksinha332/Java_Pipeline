15eaedbb6375bec2069528af1d0cbfbd14b0c77b
Abhishek Sinha <3327abhishek@gmail.com>
2025-11-19 17:05:02 +0530

    added loop

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for(int i=0; i<5; i++){
+            System.out.println(numbers[i]);
+        }
     }
 }
